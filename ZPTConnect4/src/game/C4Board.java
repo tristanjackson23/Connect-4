d031156a314220e15bf304f569aaf1ea0bbbc112
zachytdsb <zachary.yerrill17@gmail.com>
2018-05-01 13:55:26 -0400

    I tried things and mostly failed

--- ZPTConnect4/src/game/C4Board.java
+++ ZPTConnect4/src/game/C4Board.java
@@ -1,10 +1,28 @@
 package game;
 
 public class C4Board {
+	public C4Board() {
+		int[][] board = new int[6][7];
+		/*
+		 * for (int i = 0; i < board.length; i++) { for (int j = 0; j <
+		 * board[i].length; j++) { board[i][j] = 'n'; } }
+		 */
+	}
 
-	private Cell[][] board;
-	private int rows;
-	private int cols;
-
-
+	public void deposit(int colour, int column, int[][] board) {/*need runtime exception for column full*/
+		int lowest = 0;
+		for (int i = board.length - 1; i >= 0; i--) {
+			if (board[i][column] == 0) {
+				lowest = i;
+				break;
+			}
+		}
+		board[lowest][column] = colour;
+	}
+	@Override
+	public String toString(){
+		for (int i = 0; i < C4Board.board.length; i++) { for (int j = 0; j < board[i].length; j++) { 
+			board[i][j] = 'n'; }
+		}
+	}
 }
---
+++ ZPTConnect4/src/game/Cell.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Cell {
+	public Cell ( int rows, int columns){
+		public int[][] board = new int[6][7];
+	}
+
+}
