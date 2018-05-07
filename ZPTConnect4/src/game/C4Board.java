1fb271099678174bee7a501e0fbec0f8c0642048
Peter McComb <peter.mccomb@student.tdsb.on.ca>
2018-05-07 13:02:17 -0400

    cell class

--- ZPTConnect4/src/game/C4Board.java
+++ ZPTConnect4/src/game/C4Board.java
@@ -1,7 +1,19 @@
 package game;
 
+
 public class C4Board {
-	private C4CellState[][] board;
+	private C4Cell[][] board;
 	private int rows;
 	private int cols;
+
+	public Board(int aRows, int aCols) {
+	board = new C4Cell[aRows][aCols];
+	rows = aRows;
+	cols = aCols;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			board[i][j] = new C4Cell(); 
+		}
+	}
+	}
 }
---
+++ ZPTConnect4/src/game/C4Cell.java
@@ -0,0 +1,11 @@
+package game;
+
+
+
+public class C4Cell  {
+	private CellState player;
+	
+	public C4Cell(CellState ps) {
+		player = ps;
+	}
+}
