abdb5961bfe0aa4cf2e41e7d3e186468d93fa0aa
zachytdsb <zachary.yerrill17@gmail.com>
2018-05-07 13:14:49 -0400

    please for the love of doitos work

---
+++ ZPTConnect4/src/game/Board.java
@@ -0,0 +1,36 @@
+package game;
+
+public class Board {
+	private Cell[][] board;
+	private int Rows;
+	private int Cols;
+
+	public Board(int aRows, int aCols) {
+		board = new Cell[aRows][aCols];
+		/*
+		 * for (int i = 0; i < board.length; i++) { for (int j = 0; j <
+		 * board[i].length; j++) { board[i][j] = 'n'; } }
+		 */
+	}
+
+	public void deposit(int colour, int column, int[][] board) {/* need runtime exception for column full */
+		int lowest = 0;
+		for (int i = board.length - 1; i >= 0; i--) {
+			if (board[i][column] == 0) {
+				lowest = i;
+				break;
+			}
+		}
+		board[lowest][column] = colour;
+	}
+
+	@Override
+	public String toString() {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j] + ' ');
+			}
+			System.out.print("/n");
+		}
+	}
+}
---
+++ ZPTConnect4/src/game/States.java
@@ -0,0 +1,5 @@
+package game;
+
+public enum States {
+	P1, P2, E,
+}
